4d1cc2e1cca1c66bd7787b07239d4a27c77c2386
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2025-02-04 00:17:24 +0200

    test

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -15,7 +15,7 @@ export default async function Home({ params }: Props) {
   return (
     <div>
       <main className="w-full">
-        <h1 className="">dashboard</h1>
+        <h1 className="">dashboard base page</h1>
         {/* <h1 className="">{dict.pages.homePage.statistics.Books}</h1> */}
       </main>
     </div>
